d6c53422134c3c483c440b21830858d11c3310b1
Joseph M <jmedina1985@hotmail.com>
2023-03-06 16:58:41 -0800

    getting started

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ChakraProvider, theme, Alert } from '@chakra-ui/react';
+import { ChakraProvider, theme, Alert, AlertIcon } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
