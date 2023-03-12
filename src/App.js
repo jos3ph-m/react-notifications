1b5f27c13d281795d48ab3d7809c370c182ab18e
Joseph M <jmedina1985@hotmail.com>
2023-03-12 12:33:23 -0700

    Creating Chakra components

--- src/App.js
+++ src/App.js
@@ -41,11 +41,9 @@ function App() {
               Would you like to enable notifications?
             </AlertDescription>
           </Box>
-          <Button
-            colorScheme="teal"
-            size="sm"
-            onClick={enableNotifsAndClose}
-          ></Button>
+          <Button colorScheme="teal" size="sm" onClick={enableNotifsAndClose}>
+            Sure!
+          </Button>
         </Alert>
       </Container>
     </ChakraProvider>
