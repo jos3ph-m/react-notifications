1ef499614a34693d7e0f7ef024a8137bbe170e50
Joseph M <jmedina1985@hotmail.com>
2023-03-19 21:50:33 -0700

    Finishing up

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
       <Button
         colorScheme="gray"
         size="sm"
-        onClick={() => notifyUser('Thank you!')}
+        onClick={() => }
       >
         Click to show a 'Thank you!'
       </Button>
